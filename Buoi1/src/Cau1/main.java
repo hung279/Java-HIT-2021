87030380f79fb4179f2931773f6eb23170973b76
= <=>
2021-10-13 10:58:25 +0700

    BTVN

---
+++ Buoi1/src/Cau1/main.java
@@ -0,0 +1,17 @@
+package Cau1;
+
+public class main {
+    public static void main(String[] args) {
+        int m = 4, n = 5;
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i == 1 || j == 1 || i == m || j == n) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.print("\n");
+        }
+    }
+}
---
+++ Buoi1/src/Cau2/main.java
@@ -0,0 +1,21 @@
+package Cau2;
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println("*       *     *     * * * *    * * * *    *   *");
+        System.out.println("|       |    / \\    |       *  |       *   \\ /");
+        System.out.println("* ----- *   *---*   * * * *    * * * *      *");
+        System.out.println("|       |  /     \\  |          |            |");
+        System.out.println("*       * *       * *          *            *  \n\n");
+        System.out.println("*    *  ******  *       *       *");
+        System.out.println("| \\  |  |        \\     / \\     /");
+        System.out.println("*  * *  ******    *   *   *   *  ");
+        System.out.println("|   \\|  |          \\ /     \\ /");
+        System.out.println("*    *  ******      *       *    \n\n");
+        System.out.println("*   *  ******      *     * * * *");
+        System.out.println(" \\ /   |          / \\    |       *");
+        System.out.println("  *    ******    *---*   * * * *");
+        System.out.println("  |    |        /     \\  |    \\");
+        System.out.println("  *    ******  *       * *     *");
+    }
+}
